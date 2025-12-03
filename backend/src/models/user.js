5d1f096e8ef3c66ea231018eb5f28785c13af22e
Lean Gianotto <leandrogianotto@gmail.com>
2025-12-02 22:36:34 -0300

    Ultima version

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -6,7 +6,7 @@ const User = sequelize.define('User', {
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, 
     allowNull: false, 
-    // unique: true 
+     //unique: true 
     },
   password: { type: DataTypes.STRING, allowNull: false },
   role: { type: DataTypes.STRING, defaultValue: 'user' } // 'user' o 'admin'
