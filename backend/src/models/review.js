8ef0fd3c2ffaf77bfc9ef00c12da0130eabd8fe6
chiaradelpo03 <chiaradelpopolo@hotmail.com>
2026-08-02 13:47:31 -0300

    Add tableName and freezeTableName options to Review model

--- backend/src/models/review.js
+++ backend/src/models/review.js
@@ -6,7 +6,9 @@ const Review = sequelize.define('Review', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   rating: { type: DataTypes.INTEGER, allowNull: false, validate: { min: 1, max: 5 } },
   comment: { type: DataTypes.TEXT },
-}, { timestamps: true });
+}, { timestamps: true,
+   tableName: 'reviews',
+  freezeTableName: true});
 
 
 
